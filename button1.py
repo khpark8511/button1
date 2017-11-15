f5d4e15339e193026444d21f67aebf532cd51692
gg <khpark8511@naver.com>
2017-11-14 21:47:41 -0800

    birthday

--- button1.py
+++ button1.py
@@ -4,7 +4,10 @@ window =Tk()
 
 b1 = Button(window, text="GeunHwa Park")
 b2 = Button(window, text="60162482")
+b3 = Button(window, text="951127")
+
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
